1e2f699533aef9611e3739d9f847cde0dc826c3b
nk-32 <2xjpqhy46t@privaterelay.appleid.com>
2026-03-28 21:06:24 +0900

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Kota Nagai on 2026/03/28.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraTests/AuraTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuraTests.swift
+//  AuraTests
+//
+//  Created by Kota Nagai on 2026/03/28.
+//
+
+import Testing
+
+struct AuraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
